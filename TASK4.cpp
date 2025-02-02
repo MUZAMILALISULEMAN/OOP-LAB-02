7d19ec984b00029dbe327f7858d503e358b6e5ef
Muzamil Suleman <muzamilworkspace@gmail.com>
2025-02-02 02:51:56 -0800

    updated

---
+++ TASK4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int n;
+	cout<<"HOW MANY STRINGS TO BE STORED: ";
+	cin>>n;
+	
+	string *strings = new string[n];
+	
+	for(int i=0;i<n;i++){
+		
+		
+		
+	}
+	
+	
+	
+	
+	delete [] strings;
+
+
+
+	return 0;
+}
