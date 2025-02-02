fe04ab1dd138d41cb7af4c5e51f951d5a5340ece
Muzamil Suleman <muzamilworkspace@gmail.com>
2025-02-02 03:30:09 -0800

    TASK4_UPDATED

--- TASK4.cpp
+++ TASK4.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 #include <cctype>//this library has tolower(char ch) function
 using namespace std;
+
+string toLowercase(string s){
+	string str = s;
+		for(int l=0;l<str.length();l++){ // string for lowercase string 1
+			str[l] = tolower(str[l]);
+		}
+		return str;
+}
+
+
+
+
 int main() {
 
 	int n;
@@ -22,24 +34,12 @@ int main() {
 
 	}
 
-	string string1,string2;
 	string temp;
 
 
 	for(int i=0;i<n;i++){
-		
-	string1 = strings[i]; 
-	for(int l=0;l<string1.length();l++){ // string for lowercase string 1
-			string1[i] = tolower(strings[i][l]);
-		}
 	for(int j=0; j<n; j++) {
-			string2 = strings[j];
-			for(int l=0;l<string2.length();l++){ //string for lowercase string 2
-			string2[i] = tolower(strings[i][l]);
-		}
-		
-		
-		if(string1.compare(string2) < 0) { //lowercase string1 and string2 for fair comparison
+		if(toLowercase(strings[i]).compare(toLowercase(strings[j])) < 0) { //lowercase string1 and string2 for fair comparison
 			temp = strings[i];
 			strings[i] = strings[j];
 			strings[j] = temp;
